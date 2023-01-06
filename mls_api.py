916b48adfdf3211925f1862f1b8f29c295cffe02
Aaron Gabbert <aarongabbert@Aarons-Air.attlocal.net>
2023-01-06 08:41:28 -0600

    added more constants

--- mls_api.py
+++ mls_api.py
@@ -10,32 +10,28 @@ logging.basicConfig(filename='log/mls_api.log', format='%(asctime)s | %(levelnam
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
 filename = 'assets/standings.json'
 BASE_URL = 'https://sportapi.mlssoccer.com/api/'
-schedule = 'https://sportapi.mlssoccer.com/api/matches?culture=en-us&'
 standings = 'https://sportapi.mlssoccer.com/api/standings/live?isLive=true&'
 commentary = 'https://stats-api.mlssoccer.com/v1/commentaries?'
-mls = '&competitionId=98'
+MLS = '&competition=98'
+MLS_REGULAR = MLS + '&matchType=Regular'
+MLS_CUP = MLS + '&matchType=Cup'
+USOPEN_CUP = '&competition=557'
+CONCACAF_CL = '&competition=549'
+LEAGUES_CUP = '&competition=1045'
+ALL_STAR_GAME = '&competition=355'
+FRIENDLY = '&competition=34'
 # format: YYYY-MM-DD
-date_from = '&dateFrom='
-date_to = '&dateTo='
-season = '&seasonId='
+DATE_FROM = '&dateFrom='
+DATE_TO = '&dateTo='
+SEASON = '&seasonId='
 match = '&match_game_id='
 commentary_tail = '?&match_game_id=2261459&include=club&include=player&include=player_match&order_by=-commentary_period&order_by=-commentary_minute&order_by=-commentary_second&order_by=-commentary_timestamp&order_by=-commentary_opta_id&page=0&page_size=500'
-'''
-with open(filename, 'w') as f:
-    for j in r.json():
-        data.append(j)
-    string = json.dumps(data, indent=4)
-    f.write(string)
 
-def get_data(url: str):
-    r = requests.get(url)
-    return r.json()
-data = None
-with open(filename, 'r') as f:
-    data = json.load(f)
-'''
 
-def call_api(url):
+def call_api(url: str):
+    """Call the MLS API at the given url, and return the json data and status code.
+    Keyword arguments:
+    url -- the url to call"""
     headers = {
         'user-agent': USER_AGENT,
         'accept-encoding': 'gzip, deflate, br',
