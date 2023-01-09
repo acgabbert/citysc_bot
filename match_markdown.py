9a1601c08aa7be374bd20ff1c6a1ca8dbef389c0
Aaron Gabbert <aarongabbert@Aarons-Air.attlocal.net>
2023-01-08 19:23:30 -0600

    renamed match_markdown

--- match_markdown.py
+++ match_markdown.py
@@ -81,19 +81,27 @@ def post_match_thread(match_obj: match.Match):
     return None
 
 
-def get_upcoming_matches(opta_id=None, date_to=None):
+def get_upcoming_matches(date_from=None, opta_id=None):
     """Get upcoming matches from the local database."""
-    today = int(time.time())
-    if date_to is None:
-        date_to = today + 172800
-    sql = f'SELECT * FROM match WHERE time > {today} AND time < {date_to}'
+    if date_from is None:
+        date_from = int(time.time()) + 86400
+    date_to = date_from + 86400
+    sql = f'SELECT * FROM match WHERE time > {date_from} AND time < {date_to}'
     if opta_id is not None:
         sql += f' AND (home = {opta_id} OR away = {opta_id})'
     matches = util.db_query(sql)
+    if len(matches) > 0:
+        for row in matches:
+            id = row[0]
+            match_obj = match.Match(id)
+            title, markdown = pre_match_thread(match_obj)
+            print(f'Match coming up: {match_obj.opta_id}; {title}')
     return matches
 
+
+@util.time_dec(False)
 def main():
-    matches = get_upcoming_matches(date_to=1675144800)
+    matches = get_upcoming_matches(date_from=1674627098)
     print(matches)
     """
     opta_id = 2261385
@@ -101,6 +109,7 @@ def main():
     title, markdown = match_thread(match_obj)
     print(markdown)
     """
+
 
 if __name__ == '__main__':
     main()
