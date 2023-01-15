f79f7ee3c5531f13b9bb87a1ac5ae5f559047377
Aaron <gabbert.aaron@gmail.com>
2023-01-15 16:07:26 -0600

    moved player to player.py

---
+++ player.py
@@ -0,0 +1,14 @@
+"""Get player stats"""
+import mls_api as mls
+
+
+class Player(mls.MlsObject):
+    def __init__(self, opta_id, name, status, formation_place, team):
+        super().__init__(opta_id)
+        self.name = name
+        self.status = status
+        self.formation_place = formation_place
+        self.team = team
+    
+    def __str__(self):
+        return self.name
