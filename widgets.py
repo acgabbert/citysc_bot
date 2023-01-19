2a1056131fa70bf8b3372ea6af376ae04bced4c1
Aaron <gabbert.aaron@gmail.com>
2023-01-18 20:37:44 -0600

    added write_markdown

--- widgets.py
+++ widgets.py
@@ -1,5 +1,9 @@
-import mls_schedule
+import logging
+import subprocess
+
+import discord as msg
 from match import Match, get_match_data
+import mls_schedule
 import widget_markdown as md
 
 STL_CITY = 17012
@@ -18,6 +22,31 @@ def get_upcoming(opta_id):
     return matches
 
 
-if __name__ == '__main__':
+def upcoming(opta_id):
     matches = get_upcoming(STL_CITY)
-    print(md.schedule(matches))
+    markdown = md.schedule(matches)
+    return markdown
+
+
+def standings():
+    """Returns """
+
+
+def write_markdown(markdown: str, filename: str):
+    with open(filename, 'w') as f:
+        f.write(markdown)
+    changes = subprocess.run(f'git status {filename}', capture_output=True, shell=True, text=True).stdout
+    logging.debug(changes)
+    if 'Changes not staged' in changes:
+        message = f'{filename} changed.'
+        logging.info(message)
+        msg.send(f'{msg.user}\n{message}')
+    else:
+        message = f'No changes to {filename}.'
+        logging.info(message)
+        msg.send(message)
+
+
+if __name__ == '__main__':
+    markdown = upcoming()
+    print(markdown)
