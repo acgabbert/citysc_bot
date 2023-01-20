c56ddbc58993f779c4ed269274fc9a241e506fe2
Aaron <gabbert.aaron@gmail.com>
2023-01-20 07:22:19 -0600

    fixed __repr__ call

--- controller.py
+++ controller.py
@@ -91,7 +91,7 @@ def log_all_jobs():
     jobs = schedule.get_jobs()
     message = 'Currently scheduled jobs:\n'
     for job in jobs:
-        message += job.__repr__ + '\n'
+        message += job.__repr__() + '\n'
     root.info(message)
     msg.send(f'{message}')
     return jobs
