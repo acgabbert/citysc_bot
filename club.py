0ea233e8a0038f5b2fb24e5d565cd11dc3fa7126
Aaron <gabbert.aaron@gmail.com>
2023-02-06 06:48:06 -0600

    added goalscorers

--- club.py
+++ club.py
@@ -36,6 +36,7 @@ class ClubMatch(Club):
         self.recent_form = ''
         self.manager = ''
         self.lineup = []
+        self.goalscorers = []
         # TODO is this the best way to handle it? 
         # e.g. starter = key, sub = value
         self.subs = {}
