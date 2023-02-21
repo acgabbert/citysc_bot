2837f438bec96b31152e3301914fbb06f377543a
Aaron <gabbert.aaron@gmail.com>
2023-02-20 19:15:58 -0600

    removed .sh files

--- citysc_bot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/aaron/mls_api
-source /home/aaron/mls_api/env/bin/activate
-/home/aaron/mls_api/env/bin/python /home/aaron/mls_api/sched_controller.py --sub /r/stlouiscitysc &
-deactivate
