c9efde46daa0af9eda85545dc392352c43b9916c
Aaron <gabbert.aaron@gmail.com>
2023-02-20 19:12:10 -0600

    added .sh files to repo

---
+++ test_bot.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/aaron/mls_api
+source /home/aaron/mls_api/env/bin/activate
+/home/aaron/mls_api/env/bin/python /home/aaron/mls_api/sched_controller.py --sub /r/citysc_bot_test &
+deactivate
