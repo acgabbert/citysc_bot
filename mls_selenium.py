0ee1e3eb19f5d6a55254242c7db7764fe6ab9c59
Aaron <gabbert.aaron@gmail.com>
2023-07-15 08:12:24 -0500

    selenium rewrite

--- mls_selenium.py
+++ mls_selenium.py
@@ -11,29 +11,20 @@ from PIL import Image
 from config import EXE_PATH
 
 standings_url = 'https://www.mlssoccer.com/standings/2023/conference#season=2023&live=false'
-standings_tag = 'mls-c-standings__wrapper'
+standings_xpath = f"//div[@class='mls-c-standings__wrapper']"
 
 schedule_url = 'https://www.mlssoccer.com/schedule/scores#competition=all&club=17012&date='
 schedule_url_ex = 'https://www.mlssoccer.com/schedule/scores#competition=all&club=17012&date=2023-07-10'
-schedule_tag = 'mls-c-schedule__matches'
+schedule_xpath = f"//div[@class='mls-c-schedule__matches']"
 
-def config_selenium_driver(width=375, length=2800):
+def get_mls_driver(url, width=375, height=2800):
     service = ChromeService(executable_path=EXE_PATH)
     options = Options()
     options.add_argument('--headless')
     options.add_argument('--disable-notifications')
     driver = webdriver.Chrome(options=options, service=service)
-    # TODO this may need to be tweaked depending on how the screenshots turn out
-    # widths:
-    # - 325 = until PPG
-    # - 375 = until GP
-    # length = 2800 gets all of the standings
-    driver.set_window_size(width, length)
-    return driver
-
-
-def get_mls_driver(url):
-    driver = config_selenium_driver()
+    driver.set_window_size(width, height)
+    
     driver.get(url)
     # click the onetrust cookie banner and wait until it goes away
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[@id="onetrust-accept-btn-handler"]')))
@@ -43,44 +34,40 @@ def get_mls_driver(url):
     return driver
 
 
-# TODO consider changing tag to xpath since it might not always be a div@class=
-def get_standings(url=standings_url, tag=standings_tag, driver=None):
+def get_screenshot(url, outer_xpath, inner_xpath=None, title=None, driver=None):
     if driver is None:
         driver = get_mls_driver(url)
-    #write_screenshot(driver.get_screenshot_as_png(), 'whole-page')
-    elements = driver.find_elements(By.XPATH, f"//div[@class='{tag}']")
+    print(f'getting {url}\nfinding elements by {outer_xpath}')
+    elements = driver.find_elements(By.XPATH, outer_xpath)
     i = 0
+    print(f'found {len(elements)} elements')
     for element in elements:
-        # TODO find element for table title and add to filename
-        # mls-o-table__header-text 
-        title = element.find_elements(By.XPATH, '//tr[@class="mls-o-table__header-group mls-o-table__header-group--main"]')
-        title = title[i].text
-        i += 1
-        #actions = ActionChains(driver)
-        #actions.move_to_element(element).perform()
+        if inner_xpath is not None:
+            title = element.find_elements(By.XPATH, inner_xpath)
+            title = title[i].text
+            i += 1
         screenshot = element.screenshot_as_png
-        write_screenshot(screenshot, f'{title}')
+        print(f'writing {title}')
+        write_screenshot(screenshot, title)
     driver.quit()
+
+
+# TODO consider changing tag to xpath since it might not always be a div@class=
+def get_standings(url=standings_url, xpath=standings_xpath, driver=None):
+    get_screenshot(url, xpath, '//tr[@class="mls-o-table__header-group mls-o-table__header-group--main"]')
 
 
 # TODO add a "this week"/"next week" screenshot
-# url https://www.mlssoccer.com/schedule/scores#competition=all&club=17012
-def get_schedule(url=schedule_url, tag=schedule_tag, driver=None):
+# base url https://www.mlssoccer.com/schedule/scores#competition=all&club=17012&date=
+def get_schedule(url=schedule_url, xpath=schedule_xpath, driver=None):
     now = datetime.now()
     this_week_url = f'{url}{now.year}-{now.month}-{now.day}'
-    if driver is None:
-        driver = get_mls_driver(this_week_url)
-    element = driver.find_element(By.XPATH, f"//div[@class='{tag}']")
-    screenshot = element.screenshot_as_png
-    write_screenshot(screenshot, 'This Week')
-    driver.quit()
-    now = datetime.now() + timedelta(days=7)
-    next_week_url = f'{url}{now.year}-{now.month}-{now.day}'
-    driver = get_mls_driver(next_week_url)
-    element = driver.find_element(By.XPATH, f"//div[@class='{tag}']")
-    screenshot = element.screenshot_as_png
-    write_screenshot(screenshot, 'Next Week')
-    driver.quit()
+    get_screenshot(this_week_url, xpath, title="This Week")
+    next_week = datetime.now() + timedelta(days=7)
+    next_week_url = f'{url}{next_week.year}-{next_week.month}-{next_week.day}'
+    get_screenshot(next_week_url, xpath, title="Next Week")
+    pad_image(f'png/This Week-{now.month}-{now.day}.png')
+    pad_image(f'png/Next Week-{now.month}-{now.day}.png')
 
 
 def write_screenshot(data, filename):
@@ -88,6 +75,7 @@ def write_screenshot(data, filename):
     filename = f'png/{filename}-{now.month}-{now.day}.png'
     with open(filename, 'wb') as f:
         f.write(data)
+        f.close()
 
 
 def pad_image(filename):
@@ -102,5 +90,3 @@ def pad_image(filename):
 if __name__ == '__main__':
     get_standings()
     get_schedule()
-    pad_image('png/This Week-7-14.png')
-    pad_image('png/Next Week-7-14.png')
