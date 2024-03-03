0a863a8fe8dc55e9b998fba2dd31f2d0c0d685b5
Aaron <gabbert.aaron@gmail.com>
2024-03-03 09:16:48 -0600

    added basic template

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from datetime import timedelta
-from flask import Flask
+from flask import Flask, render_template
 import json
 
 import citysc_bot.mls_schedule as mls_schedule
@@ -8,11 +8,12 @@ import citysc_bot.mls_schedule as mls_schedule
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def hello_world():
     date_format = '%Y-%m-%d'
     date_from = datetime.now()
     date_to = date_from + timedelta(days=14)
     date_from = date_from.strftime(date_format)
     date_to = date_to.strftime(date_format)
-    retval = mls_schedule.get_schedule(team=17012, date_from=date_from, date_to=date_to)
-    return json.dumps(retval, indent=2)
+    schedule = mls_schedule.get_schedule(team=17012, date_from=date_from, date_to=date_to)
+    return render_template('index.html', schedule=schedule)
